ab20027c78b02995ccb08c65365c87f03f042a46
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2023-05-23 16:12:07 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,11 +7,13 @@ terraform {
   }
 }
 
+# we initialize a provider
 provider "aws" {
   profile = "default"
   region  = "eu-central-1"
 }
 
+# we create a new provider using alias london
 provider "aws" {
   alias  = "london"
   region = "eu-west-2"
